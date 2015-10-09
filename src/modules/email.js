985aad341caa760e14d385ae122e71dcc77387d6
Kai Curry <kai@sundaysenergy.com>
2015-10-09 14:02:09 -0500

    fake async action

--- src/modules/email.js
+++ src/modules/email.js
@@ -1,4 +1,6 @@
 const UPDATE = 'email/UPDATE';
+const ASYNC_VALIDATION = 'email/ASYNC_VALIDATION';
+
 import emailValidate from './emailValidate';
 
 const initialState = {
@@ -17,6 +19,7 @@ const initialState = {
 
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
+    case ASYNC_VALIDATION:
     case UPDATE:
       return {
         ...state,
@@ -27,10 +30,46 @@ export default function reducer(state = initialState, action = {}) {
   }
 }
 
+function shouldAsyncValidate(payload) {
+  if (payload.hasErrors) {
+    return false;
+  }
+  return true;
+}
+
+function handleAsyncValidation({isValid}) {
+  // @TODO Do something with invalid result or error.
+  return {
+    type: ASYNC_VALIDATION,
+    payload: {
+      asyncValidating: false,
+      asyncValid: isValid
+    }
+  };
+}
+
 // Update the curent value. Update various properties based on validation.
 export function onChange(email) {
-  return {
+  // Get initial validation results.
+  const syncValidateResults = emailValidate(email);
+  // Paint the basic action.
+  const action = {
     type: UPDATE,
-    payload: emailValidate(email)
+    payload: syncValidateResults
   };
+  // Decide if we need an async validation.
+  if (shouldAsyncValidate(syncValidateResults)) {
+    // Adjust the payload before sending the first action.
+    action.payload.asyncValidating = true;
+    // Return a function with the first param dispatch.
+    return dispatch => {
+      // Send the initial action.
+      dispatch(action);
+      setTimeout(() => {
+        dispatch(handleAsyncValidation({isValid: true}));
+      }, 1000);
+    };
+  }
+  // Send action object. No async processing.
+  return action;
 }
