d2917d3170c40a08823a0302ebd04da1a0012b4a
Kai Curry <kai@sundaysenergy.com>
2015-10-12 11:53:29 -0500

    only pass onSubmit to input if not errors

--- src/input/Field.js
+++ src/input/Field.js
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 import Input from './Input';
 import Help from './Help';
 
-function Field({className, hasErrors, status, helpMsg, errorMsg, id, children, ...other}) {
+function Field({className, hasErrors, status, helpMsg, errorMsg, id, children, onSubmit, ...other}) {
   return (
     <div className={classNames('editable-form col-md-9', status, className)}>
       <div className="editable-row">
@@ -12,6 +12,7 @@ function Field({className, hasErrors, status, helpMsg, errorMsg, id, children, .
           {...other}
           className="form-control"
           id={id}
+          onSubmit={hasErrors ? undefined : onSubmit}
         />
         { children }
       </div>
@@ -21,7 +22,7 @@ function Field({className, hasErrors, status, helpMsg, errorMsg, id, children, .
 }
 
 Field.propTypes = {
-  id: PropTypes.string.isRequired
+  id: PropTypes.string.isRequired,
 };
 
 export default Field;
