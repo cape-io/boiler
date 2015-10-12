5e7c96d6436d104c30e2fd7b9d23159135d43c79
Kai Curry <kai@sundaysenergy.com>
2015-10-12 11:54:05 -0500

    Add buttons if include onSubmit or onClose.

---
+++ src/input/Buttons.js
@@ -0,0 +1,48 @@
+import React, { Component, PropTypes } from 'react';
+import Icon from '../Icon';
+
+class EditableButtons extends Component {
+  handleSubmit(event) {
+    const {onSubmit} = this.props;
+    event.preventDefault();
+    onSubmit();
+  }
+
+  render() {
+    const {onClose, disabled} = this.props;
+
+    let CloseButton = false;
+    if (onClose) {
+      CloseButton = (
+        <button
+          className="btn btn-sm editable-close"
+          type="button"
+          onClick={onClose}
+        >
+          <Icon symbol="ban-circle" />
+        </button>
+      );
+    }
+
+    return (
+      <div className="editable-buttons">
+        <button
+          className="btn btn-sm editable-submit"
+          disabled={disabled}
+          type="submit"
+          onClick={this.handleSubmit.bind(this)}
+        >
+          <Icon symbol="ok" />
+        </button>
+        { CloseButton }
+      </div>
+    );
+  }
+}
+
+EditableButtons.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+};
+export default EditableButtons;
--- src/input/FieldGroup.js
+++ src/input/FieldGroup.js
@@ -2,22 +2,32 @@ import React, { PropTypes } from 'react';
 
 import Field from './Field';
 import FormGroup from './FormGroup';
+import Buttons from './Buttons';
 
 function FieldGroup({id, label, editable, required, status, children, ...other}) {
+  const {onClose, onSubmit, hasErrors, asyncValidating, value} = other;
+  let ButtonsEl = false;
+  if (onClose || onSubmit) {
+    const disabled = hasErrors || asyncValidating || (required && !value);
+    ButtonsEl = <Buttons onClose={onClose} onSubmit={onSubmit} disabled={disabled} />;
+  }
   return (
     <FormGroup id={id} label={label} editable={editable} required={required} status={status}>
-      <Field id={id} status={status} {...other} />
+      <Field id={id} status={status} {...other}>
+        { ButtonsEl }
+      </Field>
       { children }
     </FormGroup>
   );
 }
 
 FieldGroup.propTypes = {
-  id: PropTypes.string.isRequired
+  id: PropTypes.string.isRequired,
+  status: PropTypes.string,
 };
 FormGroup.defaultProps = {
   editable: true,
-  required: false
+  required: false,
 };
 
 export default FieldGroup;
