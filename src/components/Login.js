376f7fd2e283170bd38fa1c68436abc60bfc59c5
Kai Curry <kai@sundaysenergy.com>
2015-10-13 11:26:02 -0500

    provider links

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,29 @@
 import React, { PropTypes } from 'react';
 
+import ProviderLinks from './ProviderLinks';
+
 // Basic suggestion button.
-function Login({photo, providers}) {
+function Login({photo, providers, email, onClick}) {
   let PhotoEl = false;
   if (photo) {
     PhotoEl = <img src={photo.previewUrl} />;
   }
+  const loginProviders = [
+    {type: 'email', label: `Email ${email}`, onClick},
+  ];
+  providers.forEach(provider => {
+    if (provider === 'google') {
+      loginProviders.push({
+        type: 'google',
+        label: 'Google',
+        link: 'google',
+      });
+    }
+  });
   return (
     <div>
       { PhotoEl }
-      <pre>{ JSON.stringify(providers, null, 2) }</pre>
+      <pre>{ JSON.stringify(loginProviders, null, 2) }</pre>
     </div>
   );
 }
