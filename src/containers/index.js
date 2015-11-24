b6a4f292cd199e7b96042ea4125738d2ecb67662
Kai Curry <kai@cape.io>
2015-11-24 00:07:36 -0600

    more

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,7 +1,9 @@
 // Components acting as a container.
-export App from '../components/App';
+export App from './App'
 // Redux connection containers.
-export Counter from './Counter';
-export JoinLogin from './JoinLogin/JoinLogin';
-export JoinLoginEmail from './JoinLogin/Email';
-export List from './List';
+export Counter from './Counter'
+export JoinLogin from './JoinLogin/JoinLogin'
+export JoinLoginEmail from './JoinLogin/Email'
+export List from './List'
+export RepoPage from './RepoPage'
+export UserPage from './UserPage'
