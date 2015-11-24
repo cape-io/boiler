05fd9224086970b737fbc3c84c977e7804a5097b
Kai Curry <kai@cape.io>
2015-11-24 01:33:34 -0600

    cleanup

--- src/components/JoinLogin.js
+++ src/components/JoinLogin.js
@@ -1,27 +1,7 @@
-import React, { PropTypes } from 'react';
-
-// import FieldGroup from '../input/FieldGroup';
-// import Login from './Login';
+import React, { PropTypes } from 'react'
 
 // Basic suggestion button.
-function JoinLogin({leadMsg, headerMsg, children, ...rest}) {
-  // const { asyncValid, user, sendToken, sendingToken, sentTokenSuccess, ...other } = props;
-  //
-  // let ChildEl = false;
-  // if (asyncValid && user) {
-  //   headerMsg = 'Login';
-  //   leadMsg = 'Select a login method';
-  //   ChildEl = (
-  //     <Login
-  //       sendingToken={sendingToken}
-  //       sentTokenSuccess={sentTokenSuccess}
-  //       onClick={sendToken}
-  //       {...user}
-  //     />
-  //   );
-  // } else {
-  //   ChildEl = <FieldGroup {...other} />;
-  // }
+function JoinLogin({ leadMsg, headerMsg, children, ...rest }) {
 
   return (
     <div>
@@ -29,14 +9,14 @@ function JoinLogin({leadMsg, headerMsg, children, ...rest}) {
       <p className="lead">{ leadMsg }</p>
       { children }
     </div>
-  );
+  )
 }
 
 JoinLogin.propTypes = {
   leadMsg: PropTypes.string.isRequired,
   headerMsg: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
-};
-JoinLogin.defaultProps = {};
+}
+JoinLogin.defaultProps = {}
 
-export default JoinLogin;
+export default JoinLogin
