0dcec4baf668b82537d932094d3bd3f9b7242ea5
Kai Curry <kai@cape.io>
2015-12-18 13:15:09 -0600

    loaders instead of loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -21,7 +21,7 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
       include: path.join(__dirname, 'src'),
-      loader: 'babel',
+      loaders: ['babel'],
       query: {
           cacheDirectory: true,
           presets: ['es2015', 'stage-0', 'react'],
